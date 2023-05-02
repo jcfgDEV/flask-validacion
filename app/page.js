b5d3f5f38c6817f080cc6beee028ac432b17bc77
Juan Gouveia <85375414+jcfgDEV@users.noreply.github.com>
2023-05-03 00:53:17 +0100

    Endpoint for fetch

--- app/page.js
+++ app/page.js
@@ -38,7 +38,7 @@ export default function Page() {
 
     const handler = async (e) => {
         e.preventDefault();
-        const res = await fetch('http://localhost:5000/add', {
+        const res = await fetch('https://flask-validate-extenso-backend.vercel.app/add', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
